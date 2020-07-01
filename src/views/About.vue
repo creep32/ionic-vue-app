0b981300d00e31418174096a43a684de2e702ffd
Shintaro Kurihara <kuri.creep.kuri@gmail.com>
2020-07-01 07:33:22 +0000

    modify

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,8 +17,41 @@
           <ion-badge color="success" slot="end">5 Days</ion-badge>
         </ion-item>
       </ion-list>
+
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon name="camera" />
+        </ion-fab-button>
+      </ion-fab>
+
     </ion-content>
   </ion-page>
 </template>
 
+<script>
+import { camera, trash, close } from 'ionicons/icons';
 
+import {
+  Plugins,
+  CameraSource,
+  CameraResultType
+} from "@capacitor/core";
+const { Camera } = Plugins;
+
+export default {
+  name: 'About',
+  props: {
+    msg: String
+  },
+  methods: {
+    takePhoto: async () => {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Prompt
+      });
+    },
+  },
+}
+</script>
